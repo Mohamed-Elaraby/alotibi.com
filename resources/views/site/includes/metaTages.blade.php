0583853e32a57f06b4539cc9c67b17a79a1e6063
Mohamed <mido.b333@gmail.com>
2022-02-05 15:55:06 +0200

    add meta tag robots

--- resources/views/site/includes/metaTages.blade.php
+++ resources/views/site/includes/metaTages.blade.php
@@ -4,3 +4,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="{{ $setting? $setting -> description: null }}">
 <meta name="keywords" content="{{ $setting? $setting -> keywords: null }}">
+<meta name="robots" content="index,follow">
